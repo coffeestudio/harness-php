3cfc09e3844fd4100392c37a5a9a5d95f9a55b8b
Art <art@shesha.ru>
2015-07-02 16:50:17 +0300

    IAdminViewDAO: new methods

--- Harness/IAdminViewDAO.php
+++ Harness/IAdminViewDAO.php
@@ -3,5 +3,8 @@ namespace CoffeeStudio\Harness;
 
 interface IAdminViewDAO extends IDAO
 {
+    public function get($id);
     public function getList();
+    public function listViewFields();
+    public function editViewFields();
 }
